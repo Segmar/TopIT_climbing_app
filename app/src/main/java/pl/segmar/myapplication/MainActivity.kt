6566a4be55afce74f54183b227c176617ce2d89c
Segmar <kacper.tomkiewicz@o2.pl>
2023-10-30 23:09:39 +0100

    database added

--- app/src/main/java/pl/segmar/myapplication/MainActivity.kt
+++ app/src/main/java/pl/segmar/myapplication/MainActivity.kt
@@ -4,12 +4,17 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val firebase : DatabaseReference = FirebaseDatabase.getInstance().getReference()
+
 
         val showNewAccLayout = findViewById<Button>(R.id.newaccountbutton)
         showNewAccLayout.setOnClickListener{
